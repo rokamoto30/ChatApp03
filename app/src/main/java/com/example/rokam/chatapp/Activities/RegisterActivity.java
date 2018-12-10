d23cde2af1c09b3fda102446e0eccc8099b9b404
aryaman_joshi <aryaman5@illinois.edu>
2018-12-09 21:51:44 -0600

    this is working and finsished but we might change the UI

--- app/src/main/java/com/example/rokam/chatapp/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/rokam/chatapp/Activities/RegisterActivity.java
@@ -11,6 +11,9 @@ import com.example.rokam.chatapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
     private Button button;
+    private Button button2;
+    private Button button3;
+    private Button button4;
 
 
     @Override
@@ -24,10 +27,44 @@ public class RegisterActivity extends AppCompatActivity {
                 openHome();
             }
         });
+
+        button2 = (Button) findViewById(R.id.btn2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHome2();
+            }
+        });
+        button3 = (Button) findViewById(R.id.btn3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHome3();
+            }
+        });
+        button4 = (Button) findViewById(R.id.btn4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHome4();
+            }
+        });
     }
     public void openHome() {
-        findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
         Intent intent = new Intent(this, home.class);
         startActivity(intent);
     }
+    public void openHome2() {
+        Intent intent2 = new Intent(this, home2.class);
+        startActivity(intent2);
+    }
+    public void openHome3() {
+        Intent intent3 = new Intent(this, home3.class);
+        startActivity(intent3);
+    }
+    public void openHome4() {
+        Intent intent4 = new Intent(this, home4.class);
+        startActivity(intent4);
+    }
+
 }
---
+++ app/src/main/java/com/example/rokam/chatapp/Activities/home4.java
@@ -0,0 +1,116 @@
+package com.example.rokam.chatapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.rokam.chatapp.R;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class home4 extends AppCompatActivity {
+    EditText edit_title, edit_content;
+    Button btn_post;
+    RecyclerView recyclerView;
+
+    //Firebase
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    FirebaseRecyclerOptions<Post> options;
+    FirebaseRecyclerAdapter<Post, MyRecyclerView> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home4);
+        edit_content = (EditText)findViewById(R.id.txtPost4);
+        edit_title = (EditText)findViewById(R.id.Name4);
+        btn_post = (Button)findViewById(R.id.send4);
+        recyclerView = (RecyclerView)findViewById(R.id.RecyclerView4);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("MATH 231");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                displayComment();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        btn_post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postComment();
+            }
+        });
+
+        displayComment();
+    }
+
+    @Override
+    protected void onStop() {
+        if(adapter != null)
+            adapter.stopListening();
+        super.onStop();
+    }
+
+    private void postComment() {
+        String title = edit_title.getText().toString();
+        String content = edit_content.getText().toString();
+
+        Post post = new Post(title, content);
+
+        databaseReference.push() //use method to create new id of comment
+                .setValue(post);
+
+
+        adapter.notifyDataSetChanged();
+    }
+    private void displayComment() {
+        options =
+                new FirebaseRecyclerOptions.Builder<Post>()
+                        .setQuery(databaseReference, Post.class)
+                        .build();
+        adapter =
+                new FirebaseRecyclerAdapter<Post, MyRecyclerView>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull MyRecyclerView holder, int position, @NonNull final Post model) {
+                        holder.txt_title.setText(model.getTitle());
+                        holder.txt_comment.setText(model.getContent());
+                    }
+
+                    @NonNull
+                    @Override
+                    public MyRecyclerView onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                        View itemView = LayoutInflater.from(getBaseContext()).inflate(R.layout.post_item, viewGroup, false);
+                        return new MyRecyclerView(itemView);
+                    }
+                };
+
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+
+    }
+}
